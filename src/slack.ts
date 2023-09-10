77faf803b7887897f72c022aac86e04dbca6955e
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-09-10 12:23:31 +0200

    project updates

--- src/slack.ts
+++ src/slack.ts
@@ -1,137 +1,52 @@
-import type { Handler } from '@netlify/functions';
+import type { Handler } from "@netlify/functions";
 
-import { parse } from 'querystring';
-import { blocks, modal, slackApi, verifySlackRequest } from './util/slack';
-import { saveItem } from './util/notion';
+import { parse } from "querystring";
+import { slackApi } from "./util/slack";
 
-async function handleSlashCommand(payload: SlackSlashCommandPayload) {
-	switch (payload.command) {
-		case '/foodfight':
-			const response = await slackApi(
-				'views.open',
-				modal({
-					id: 'foodfight-modal',
-					title: 'Start a food fight!',
-					trigger_id: payload.trigger_id,
-					blocks: [
-						blocks.section({
-							text: 'The discourse demands food drama! *Send in your spiciest food takes so we can all argue about them and feel alive.*',
-						}),
-						blocks.input({
-							id: 'opinion',
-							label: 'Deposit your controversial food opinions here.',
-							placeholder:
-								'Example: peanut butter and mayonnaise sandwiches are delicious!',
-							initial_value: payload.text ?? '',
-							hint: 'What do you believe about food that people find appalling? Say it with your chest!',
-						}),
-						blocks.select({
-							id: 'spice_level',
-							label: 'How spicy is this opinion?',
-							placeholder: 'Select a spice level',
-							options: [
-								{ label: 'mild', value: 'mild' },
-								{ label: 'medium', value: 'medium' },
-								{ label: 'spicy', value: 'spicy' },
-								{ label: 'nuclear', value: 'nuclear' },
-							],
-						}),
-					],
-				}),
-			);
+async function handleSlackSlashCommand(payload: SlackSlashCommandPayload) {
+  switch (payload.command) {
+    case "/taskfight":
+      const joke = await fetch("https://icanhazdadjoke.com", {
+        headers: { accept: "text/plain" },
+      });
+      const response = await slackApi("chat.postMessage", {
+        channel: payload.channel_id,
+        // text: "Things are happening!",
+        text: await joke.text(),
+      });
 
-			if (!response.ok) {
-				console.log(response);
-			}
+      if (!response.ok) {
+        console.log(response);
+      }
 
-			break;
+      break;
 
-		default:
-			return {
-				statusCode: 200,
-				body: `Command ${payload.command} is not recognized`,
-			};
-	}
+    default:
+      return {
+        statusCode: 200,
+        body: `command ${payload.command} is not recognised`,
+      };
+  }
 
-	return {
-		statusCode: 200,
-		body: '',
-	};
-}
-
-async function handleInteractivity(payload: SlackModalPayload) {
-	const callback_id = payload.callback_id ?? payload.view.callback_id;
-
-	switch (callback_id) {
-		case 'foodfight-modal':
-			const data = payload.view.state.values;
-			const fields = {
-				opinion: data.opinion_block.opinion.value,
-				spiceLevel: data.spice_level_block.spice_level.selected_option.value,
-				submitter: payload.user.name,
-			};
-
-			await saveItem(fields);
-
-			await slackApi('chat.postMessage', {
-				channel: 'C0438E823SP',
-				text: `Oh dang, y’all! :eyes: <@${payload.user.id}> just started a food fight with a ${fields.spiceLevel} take:\n\n*${fields.opinion}*\n\n...discuss.`,
-			});
-			break;
-
-		case 'start-food-fight-nudge':
-			const channel = payload.channel?.id;
-			const user_id = payload.user.id;
-			const thread_ts = payload.message.thread_ts ?? payload.message.ts;
-
-			await slackApi('chat.postMessage', {
-				channel,
-				thread_ts,
-				text: `Hey <@${user_id}>, an opinion like this one deserves a heated public debate. Run the \`/foodfight\` slash command in a main channel to start one!`,
-			});
-
-			break;
-
-		default:
-			console.log(`No handler defined for ${payload.view.callback_id}`);
-			return {
-				statusCode: 400,
-				body: `No handler defined for ${payload.view.callback_id}`,
-			};
-	}
-
-	return {
-		statusCode: 200,
-		body: '',
-	};
+  return {
+    statusCode: 200,
+    body: "",
+  };
 }
 
 export const handler: Handler = async (event) => {
-	const valid = verifySlackRequest(event);
+  // TODO: validate the slack request
 
-	if (!valid) {
-		console.error('invalid request');
+  // TODO: handle the slash commands
+  const body = parse(event.body ?? "") as SlackPayload;
+  if (body.command) {
+    return handleSlackSlashCommand(body as SlackSlashCommandPayload);
+  }
 
-		return {
-			statusCode: 400,
-			body: 'invalid request',
-		};
-	}
+  // TODO: handle interactivity (e.g - context commands, modals)
 
-	const body = parse(event.body ?? '') as SlackPayload;
-
-	if (body.command) {
-		return handleSlashCommand(body as SlackSlashCommandPayload);
-	}
-
-	// TODO handle interactivity (e.g. context commands, modals)
-	if (body.payload) {
-		const payload = JSON.parse(body.payload);
-		return handleInteractivity(payload);
-	}
-
-	return {
-		statusCode: 200,
-		body: 'TODO: handle Slack commands and interactivity',
-	};
+  return {
+    statusCode: 200,
+    body: "TODO: handle slack commands and interactivity",
+  };
 };
--- src/util/notion.ts
+++ src/util/notion.ts
@@ -1,70 +1,70 @@
-export async function notionApi(endpoint: string, body: {}) {
-	const res = await fetch(`https://api.notion.com/v1${endpoint}`, {
-		method: 'POST',
-		headers: {
-			accept: 'application/json',
-			authorization: `Bearer ${process.env.NOTION_SECRET}`,
-			'Notion-Version': '2022-06-28',
-			'content-type': 'application/json',
-		},
-		body: JSON.stringify(body),
-	}).catch((err) => console.error(err));
+// export async function notionApi(endpoint: string, body: {}) {
+// 	const res = await fetch(`https://api.notion.com/v1${endpoint}`, {
+// 		method: 'POST',
+// 		headers: {
+// 			accept: 'application/json',
+// 			authorization: `Bearer ${process.env.NOTION_SECRET}`,
+// 			'Notion-Version': '2022-06-28',
+// 			'content-type': 'application/json',
+// 		},
+// 		body: JSON.stringify(body),
+// 	}).catch((err) => console.error(err));
 
-	if (!res || !res.ok) {
-		console.error(res);
-	}
+// 	if (!res || !res.ok) {
+// 		console.error(res);
+// 	}
 
-	const data = await res?.json();
+// 	const data = await res?.json();
 
-	return data;
-}
+// 	return data;
+// }
 
-export async function getNewItems(): Promise<NewItem[]> {
-	const notionData = await notionApi(
-		`/databases/${process.env.NOTION_DATABASE_ID}/query`,
-		{
-			filter: {
-				property: 'Status',
-				status: {
-					equals: 'new',
-				},
-			},
-			page_size: 100,
-		},
-	);
+// export async function getNewItems(): Promise<NewItem[]> {
+// 	const notionData = await notionApi(
+// 		`/databases/${process.env.NOTION_DATABASE_ID}/query`,
+// 		{
+// 			filter: {
+// 				property: 'Status',
+// 				status: {
+// 					equals: 'new',
+// 				},
+// 			},
+// 			page_size: 100,
+// 		},
+// 	);
 
-	const openItems = notionData.results.map((item: NotionItem) => {
-		return {
-			opinion: item.properties.opinion.title[0].text.content,
-			spiceLevel: item.properties.spiceLevel.select.name,
-			status: item.properties.Status.status.name,
-		};
-	});
+// 	const openItems = notionData.results.map((item: NotionItem) => {
+// 		return {
+// 			opinion: item.properties.opinion.title[0].text.content,
+// 			spiceLevel: item.properties.spiceLevel.select.name,
+// 			status: item.properties.Status.status.name,
+// 		};
+// 	});
 
-	return openItems;
-}
+// 	return openItems;
+// }
 
-export async function saveItem(item: NewItem) {
-	const res = await notionApi('/pages', {
-		parent: {
-			database_id: process.env.NOTION_DATABASE_ID,
-		},
-		properties: {
-			opinion: {
-				title: [{ text: { content: item.opinion } }],
-			},
-			spiceLevel: {
-				select: {
-					name: item.spiceLevel,
-				},
-			},
-			submitter: {
-				rich_text: [{ text: { content: `@${item.submitter} on Slack` } }],
-			},
-		},
-	});
+// export async function saveItem(item: NewItem) {
+// 	const res = await notionApi('/pages', {
+// 		parent: {
+// 			database_id: process.env.NOTION_DATABASE_ID,
+// 		},
+// 		properties: {
+// 			opinion: {
+// 				title: [{ text: { content: item.opinion } }],
+// 			},
+// 			spiceLevel: {
+// 				select: {
+// 					name: item.spiceLevel,
+// 				},
+// 			},
+// 			submitter: {
+// 				rich_text: [{ text: { content: `@${item.submitter} on Slack` } }],
+// 			},
+// 		},
+// 	});
 
-	if (!res.ok) {
-		console.log(res);
-	}
-}
+// 	if (!res.ok) {
+// 		console.log(res);
+// 	}
+// }
